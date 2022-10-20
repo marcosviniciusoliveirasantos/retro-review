44f4aadae470b531ea4b59e6e7a85bff2779cc54
MARCOS VINICIUS DE OLIVEIRA SANTOS <marcosviniciussantos@alunos.utfpr.edu.br>
2022-10-20 19:48:54 -0300

    Criação dos componentes cabecalho e rodape

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AutoriaComponent } from './autoria/autoria.component';
 import { AcessoComponent } from './acesso/acesso.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AutoriaComponent,
-    AcessoComponent
+    AcessoComponent,
+    CabecalhoComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
