005148bdcbe312f5993083349b408df7dd65d9e3
MARCOS VINICIUS DE OLIVEIRA SANTOS <marcosviniciussantos@alunos.utfpr.edu.br>
2022-10-20 23:24:24 -0300

    Design do cabeçalho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as M from 'materialize-css';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'Retro Review';
+  @ViewChild('mobile') sidenav?: ElementRef;
+
+  ngAfterViewInit(): void {
+    M.Sidenav.init(this.sidenav?.nativeElement);
+  }
 }
