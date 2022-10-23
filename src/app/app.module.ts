8c164fb37571e79440250986e9ecc74abc8f7c8b
MARCOS VINICIUS DE OLIVEIRA SANTOS <marcosviniciussantos@alunos.utfpr.edu.br>
2022-10-23 11:46:30 -0300

    Limpeza de código - remoção dos componentes da atividade 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AutoriaComponent } from './autoria/autoria.component';
-import { AcessoComponent } from './acesso/acesso.component';
 import { CabecalhoComponent } from './cabecalho/cabecalho.component';
 import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AutoriaComponent,
-    AcessoComponent,
     CabecalhoComponent,
     RodapeComponent
   ],
