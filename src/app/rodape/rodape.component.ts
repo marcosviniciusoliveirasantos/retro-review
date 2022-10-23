e52f058275884393f2f03bcd3f62e7f100ad022c
MARCOS VINICIUS DE OLIVEIRA SANTOS <marcosviniciussantos@alunos.utfpr.edu.br>
2022-10-23 12:09:54 -0300

    Uso do interpolation no rodapé

--- src/app/rodape/rodape.component.ts
+++ src/app/rodape/rodape.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-rodape',
   templateUrl: './rodape.component.html',
-  styleUrls: ['./rodape.component.css']
+  styleUrls: ['./rodape.component.css'],
 })
 export class RodapeComponent implements OnInit {
+  autoria: String;
+  copyright: String;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.autoria = 'Desenvolvido por Marcos Vinicius Santos';
+    this.copyright = `@ ${new Date().getFullYear()} Copyright - RetroReview`;
   }
 
+  ngOnInit(): void {}
 }
