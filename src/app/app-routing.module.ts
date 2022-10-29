a0689364c944a832aa582e35aeb1d83cce6790f7
MARCOS VINICIUS DE OLIVEIRA SANTOS <marcosviniciussantos@alunos.utfpr.edu.br>
2022-10-29 09:07:17 -0300

    Cadastro das rotas no app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroReviewComponent } from './cadastro-review/cadastro-review.component';
+import { CadastroUsuarioComponent } from './cadastro-usuario/cadastro-usuario.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'novo', component: CadastroUsuarioComponent },
+  { path: 'review', component: CadastroReviewComponent },
+  { path: '', component: InicioComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
