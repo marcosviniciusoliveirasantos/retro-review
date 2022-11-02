701611655d8ff9d7a22948b8efd64c5d568bd948
MARCOS VINICIUS DE OLIVEIRA SANTOS <marcosviniciussantos@alunos.utfpr.edu.br>
2022-11-02 19:09:36 -0300

    Criação da classe Usuario

---
+++ src/app/usuario.ts
@@ -0,0 +1,13 @@
+export class Usuario {
+  id: number;
+  nome: string;
+  email: string;
+  senha: string;
+
+  constructor() {
+    this.id = 0;
+    this.nome = '';
+    this.email = '';
+    this.senha = '';
+  }
+}
