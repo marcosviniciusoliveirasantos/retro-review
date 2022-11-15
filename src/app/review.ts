2ad5b9cbcd993daabb25f1f68d198ba8a963d2d3
MARCOS VINICIUS DE OLIVEIRA SANTOS <marcosviniciussantos@alunos.utfpr.edu.br>
2022-11-15 10:46:14 -0300

    Configuração do json server

--- src/app/review.ts
+++ src/app/review.ts
@@ -13,27 +13,3 @@ export class Review {
     this.autor = autor;
   }
 }
-
-export const dadosTeste: Review[] = [
-  {
-    id: 1,
-    jogo: 'Jogo 1',
-    nota: '10',
-    analise: 'Review do Jogo 1 pelo Autor 1',
-    autor: 'Autor 1',
-  },
-  {
-    id: 2,
-    jogo: 'Jogo 2',
-    nota: '8',
-    analise: 'Review do Jogo 2 pelo Autor 2',
-    autor: 'Autor 2',
-  },
-  {
-    id: 3,
-    jogo: 'Jogo 3',
-    nota: '9',
-    analise: 'Review do Jogo 3 pelo Autor 3',
-    autor: 'Autor 3',
-  },
-];
